b2417663f092e393e0ab8aba428566bd415d643c
Hubert Annamaria <hubert.annamaria@tigra.hu>
2023-12-01 17:37:33 +0100

    day1 fix nullability

--- 2023/day1/day1.kt
+++ 2023/day1/day1.kt
@@ -17,11 +17,11 @@ fun main() {
 
   File("2023/day1/input1.txt").forEachLine { line ->
     val firstDigits = getFirstDigits(line, letterDigitMap)
-    val partOneFirstDigit: Int = firstDigits.first!!
-    val partTwoFirstDigit: Int = firstDigits.second!!
+    val partOneFirstDigit: Int = firstDigits.first
+    val partTwoFirstDigit: Int = firstDigits.second
     val lastDigits = getFirstDigits(line.reversed(), letterDigitMap.mapKeys { it.key.reversed() })
-    val partOneLastDigit: Int = lastDigits.first ?: partOneFirstDigit
-    val partTwoLastDigit: Int = lastDigits.second ?: partTwoFirstDigit
+    val partOneLastDigit: Int = lastDigits.first
+    val partTwoLastDigit: Int = lastDigits.second
 
     partOneSumOfCalibrationValues += partOneFirstDigit * 10 + partOneLastDigit
     partTwoSumOfCalibrationValues += partTwoFirstDigit * 10 + partTwoLastDigit
@@ -31,21 +31,21 @@ fun main() {
   println("Answer for Part Two: $partTwoSumOfCalibrationValues")
 }
 
-private fun getFirstDigits(line: String, letterDigitMap: Map<String, Int>): Pair<Int?, Int?> {
-  var firstDigit1: Int? = null
-  var firstDigit2: Int? = null
+private fun getFirstDigits(line: String, letterDigitMap: Map<String, Int>): Pair<Int, Int> {
+  var partOneFirstDigit = 0
+  var partTwoFirstDigit = 0
   var endIdx = 1
   var startIdx = 0
-  while (startIdx < line.length && firstDigit1 == null) {
-    while (endIdx <= line.length && firstDigit1 == null) {
+  while (startIdx < line.length && partOneFirstDigit == 0) {
+    while (endIdx <= line.length && partOneFirstDigit == 0) {
       val substring = line.substring(startIdx, endIdx)
-      if (letterDigitMap.containsKey(substring) && firstDigit2 == null) {
-        firstDigit2 = letterDigitMap[substring]!!.toInt()
+      if (letterDigitMap.containsKey(substring) && partTwoFirstDigit == 0) {
+        partTwoFirstDigit = letterDigitMap[substring]!!.toInt()
         startIdx = endIdx
       } else if (substring.length == 1 && substring[0].isDigit()) {
-        firstDigit1 = substring.toInt()
-        if (firstDigit2 == null) {
-          firstDigit2 = substring.toInt()
+        partOneFirstDigit = substring.toInt()
+        if (partTwoFirstDigit == 0) {
+          partTwoFirstDigit = substring.toInt()
         }
         startIdx = endIdx
       }
@@ -54,5 +54,5 @@ private fun getFirstDigits(line: String, letterDigitMap: Map<String, Int>): Pair
     startIdx += 1
     endIdx = startIdx + 1
   }
-  return Pair(firstDigit1, firstDigit2)
+  return Pair(partOneFirstDigit, partTwoFirstDigit)
 }
