0ccccb59cc4ebe10bb6769521eea69a2f5ac8ce3
eni020 <47690361+eni020@users.noreply.github.com>
2025-12-03 14:04:47 +0100

    2025 day3

---
+++ src/year2025/day3/day3.kt
@@ -0,0 +1,43 @@
+package year2025.day3
+
+import java.io.File
+import kotlin.time.measureTime
+
+fun main() {
+    val timeTaken = measureTime {
+        var partOneSum: Long = 0
+        var partTwoSum: Long = 0
+
+        val inputLines = File("src/year2025/day3/input3.txt").readLines()
+//        val inputLines = File("src/year2025/day3/sample3.txt").readLines()
+        for (line in inputLines) {
+            partOneSum += getLargestJoltage(line, 2)
+            partTwoSum += getLargestJoltage(line, 12)
+        }
+
+        println("Answer for Part One: $partOneSum")
+        println("Answer for Part Two: $partTwoSum")
+    }
+    println(timeTaken)
+}
+
+private fun getLargestJoltage(line: String, joltageLengh: Int): Long {
+    var maxJoltage: MutableList<Int> = mutableListOf(line[0].toString().toInt())
+    val offCount = line.length - joltageLengh
+    for (lineIdx in 1..<line.length) {
+        val actDigit = line[lineIdx].toString().toInt()
+
+        if (maxJoltage.size < joltageLengh) {
+            maxJoltage.add(actDigit)
+        }
+        for (joltageIdx in 0..<maxJoltage.size) {
+            if (maxJoltage[joltageIdx] < actDigit && offCount + joltageIdx >= lineIdx) {
+                maxJoltage[joltageIdx] = actDigit
+                maxJoltage = maxJoltage.take(joltageIdx + 1).toMutableList()
+                break
+            }
+        }
+    }
+
+    return maxJoltage.joinToString("").toLong()
+}
