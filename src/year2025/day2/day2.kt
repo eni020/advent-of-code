eb35f3c44afff3c7de7adff454de2885a79f4d80
eni020 <47690361+eni020@users.noreply.github.com>
2025-12-05 11:38:45 +0100

    2025 day2 replace Pair with Range

--- src/year2025/day2/day2.kt
+++ src/year2025/day2/day2.kt
@@ -11,17 +11,18 @@ fun main() {
         var partTwoSum: Long = 0
 
         val inputLines = File("src/year2025/day2/input2.txt").readLines()
-        val ranges: List<Pair<Long, Long>> = inputLines[0].split(',').map { s -> Pair(s.substringBefore('-').toLong(), s.substringAfter('-').toLong()) }
-        ranges.forEach { (left, right) ->
-            val leftStart = getFirstPart(left)
-            var rightStart = getFirstPart(right)
-            if (leftStart > rightStart) {
-                rightStart = rightStart * 10
+        val ranges: List<LongRange> = inputLines[0].split(',').map { s -> s.substringBefore('-').toLong()..s.substringAfter('-').toLong() }
+        ranges.forEach { range ->
+            val first = range.first
+            val last = range.last
+            var numStartRange = getFirstPart(first)..getFirstPart(last)
+            if (numStartRange.first > numStartRange.last) {
+                numStartRange = numStartRange.first..numStartRange.last * 10
             }
 
             val invalidIds: MutableSet<Long> = mutableSetOf()
             val numParts: MutableSet<String> = mutableSetOf()
-            for (numStart in leftStart..rightStart) {
+            for (numStart in numStartRange) {
                 val numPartString = numStart.toString()
                 for (endIdx in numPartString.length downTo 1) {
                     val numPart = numPartString.substring(0, endIdx)
@@ -29,18 +30,18 @@ fun main() {
                         break
                     }
                     numParts.add(numPart)
-                    val repeat = min(max(left.toString().length / endIdx, 2), right.toString().length)
+                    val repeat = min(max(first.toString().length / endIdx, 2), last.toString().length)
                     if (repeat < 2) {
                         break
                     }
                     var numString = numPart.repeat(repeat)
 
-                    if (numString.toLong() < left) {
+                    if (numString.toLong() < first) {
                         numString += numPart
                     }
 
                     val num = numString.toLong()
-                    if (num in left..right) {
+                    if (num in range) {
                         if (numString == numPart + numPart) {
                             partOneSum += num
                         }
@@ -59,8 +60,8 @@ fun main() {
 }
 
 private fun getFirstPart(num: Long): Long {
-    val leftString = num.toString()
-    val halfLength = leftString.length / 2
+    val numString = num.toString()
+    val halfLength = numString.length / 2
     val endIndex = if (halfLength != 0) halfLength else 1
-    return leftString.substring(0, endIndex).toLong()
+    return numString.substring(0, endIndex).toLong()
 }
