affc3476832b5e6ab2eba6b0b0afed933a48c67d
eni020 <47690361+eni020@users.noreply.github.com>
2025-12-06 15:21:30 +0100

    2025 day6

---
+++ src/year2025/day6/day6.kt
@@ -0,0 +1,68 @@
+package year2025.day6
+
+import java.io.File
+import kotlin.collections.reduce
+import kotlin.collections.sum
+import kotlin.time.measureTime
+
+fun main() {
+    val timeTaken = measureTime {
+        var partOneSum: Long = 0
+        var partTwoSum: Long = 0
+
+        val problems = mutableListOf<MutableList<Long>>()
+        val inputLines = File("src/year2025/day6/input6.txt").readLines().toMutableList()
+
+        for (line in inputLines) {
+            val elements = line.split(' ').map { it.trim() }.filter { it.isNotBlank() }
+            for (idx in elements.indices) {
+                problems.add(mutableListOf())
+                val element = elements[idx]
+                val num = element.toLongOrNull()
+                if (num != null) {
+                    problems[idx].add(num)
+                } else {
+                    partOneSum += handleOperation(problems[idx], element)
+                }
+            }
+        }
+
+        val operandLine = inputLines.last()
+        inputLines.remove(operandLine)
+        var previousPosition = 0
+        for (idx in 1..operandLine.length) {
+            if (idx == operandLine.length || operandLine[idx] != ' ') {
+                val nums = mutableListOf<String>()
+                for (line in inputLines) {
+                    val endIdx = if (operandLine.substring(idx, operandLine.length).trim().isEmpty()) line.length else idx - 1
+                    val linePart = line.substring(previousPosition, endIdx)
+                    for (cIdx in linePart.indices) {
+                        val char = linePart[cIdx].toString().trim()
+                        if (nums.size <= cIdx) {
+                            nums.add(char)
+                        } else {
+                            nums[cIdx] += char
+                        }
+                    }
+                }
+                partTwoSum += handleOperation(nums.map { it.toLong() }, operandLine[previousPosition].toString())
+                previousPosition = idx
+            }
+        }
+
+
+
+        println("Answer for Part One: $partOneSum")
+        println("Answer for Part Two: $partTwoSum")
+    }
+    println(timeTaken)
+}
+
+fun handleOperation(nums: List<Long>, operand: String): Long {
+    if (operand == "+") {
+        return nums.sum()
+    } else if (operand == "*") {
+        return nums.reduce { acc, num -> acc * num }
+    }
+    return 0
+}
